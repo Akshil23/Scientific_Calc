7bfd26ff27ad9f70921c42e44fe9f12f8a5a96bc
Akshil Patel <116317952+Akshil23@users.noreply.github.com>
2022-10-20 20:28:49 -0400

    Add files via upload

---
+++ Test_p.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+class Test_p 
+	{
+	    public static void main(String[] args) 
+	    {
+	        JFrame frame = new scientific_calc();
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        }
+	    
+	
+}
+
